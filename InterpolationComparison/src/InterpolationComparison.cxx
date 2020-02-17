d803f36192cab95b8234a11b3d3e1303afce26fe
Gordian Kabelitz <kabcode@gmail.com>
2020-02-17 10:53:30 +0100

    Update InterpolationComparison.cxx

--- InterpolationComparison/src/InterpolationComparison.cxx
+++ InterpolationComparison/src/InterpolationComparison.cxx
@@ -2,6 +2,8 @@
 #include <itkImageFileWriter.h>
 #include "HighPrecisionTimer.h"
 #include "itkSubtractImageFilter.h"
+#include "itkAbsImageFilter.h"
+#include "itkExtractImageFilter.h"
 #include "itkMatrixOffsetTransformBase.h"
 #include "itkMacro.h"
 #include "rtkMacro.h"
@@ -19,8 +21,12 @@ using NearestNeighborInterpolationType = itk::NearestNeighborInterpolateImageFun
 using LinearInterpolationType = itk::LinearInterpolateImageFunction<ImageType, double>;
 using BSlineInterpolationType = itk::BSplineInterpolateImageFunction<ImageType, double>;
 using GaussianInterpolationType = itk::GaussianInterpolateImageFunction<ImageType, double>;
+using ExtractImageFilterType = itk::ExtractImageFilter<ImageType, itk::Image<double, 2>>;
 
-void WriteImageFile(const ImageType::Pointer& image, std::string outputname, std::string additionalInformation = "");
+template<class TImage>
+void WriteImageFile(const typename TImage::Pointer image, std::string outputname, std::string additionalInformation = "");
+
+auto Extract2DImage(ImageType::Pointer image) ->itk::Image<double, 2>::Pointer;
 
 int main(int argc, char * argv[])
 {
@@ -51,80 +57,84 @@ int main(int argc, char * argv[])
     ResampleImageFilter->SetTransform(Transform);
 
     itk::InterpolateImageFunction<ImageType, double>::Pointer Interpolator = nullptr;
-    std::string OutputFilename{};
-    const unsigned int TYPE = 0;
 
-    switch(TYPE)
+    for (unsigned int TYPE = 0; TYPE < 4; ++TYPE)
     {
-    case 0:
-        Interpolator = NearestNeighborInterpolationType::New();
-        OutputFilename.append("NearestNeigbor");
-        break;
-    case 1:
-        Interpolator = LinearInterpolationType::New();
-        OutputFilename.append("Linear");
-        break;
-    case 2:
-        Interpolator = BSlineInterpolationType::New();
-        OutputFilename.append("BSPline");
-        break;
-    case 3:
-        Interpolator = GaussianInterpolationType::New();
-        OutputFilename.append("Gaussian");
-        break;
-    default:
-        std::cout << "Value not handled - abort";
-        return EXIT_FAILURE;
-    }
-
-    if(dynamic_cast<itk::GaussianInterpolateImageFunction<ImageType, double>*>(Interpolator.GetPointer()) != nullptr)
-    {
-        auto nInterpolator = dynamic_cast<itk::GaussianInterpolateImageFunction<ImageType, double>*>(Interpolator.GetPointer());
-        nInterpolator->SetSigma(0.8);
-        Interpolator = nInterpolator;
-    }
-
-    ResampleImageFilter->SetInterpolator(Interpolator);
-
-    std::vector<long long> durations(105);
-    for (long long& duration : durations)
-    {
+        std::string OutputFilename{};
+        switch (TYPE)
         {
-            auto Timer{ HighPrecisionTimer<TimeUnits::Milliseconds>(&duration) };
-            TRY_AND_EXIT_ON_ITK_EXCEPTION(ResampleImageFilter->Update());
+        case 0:
+            Interpolator = NearestNeighborInterpolationType::New();
+            OutputFilename.append("NearestNeigbor");
+            break;
+        case 1:
+            Interpolator = LinearInterpolationType::New();
+            OutputFilename.append("Linear");
+            break;
+        case 2:
+            Interpolator = BSlineInterpolationType::New();
+            OutputFilename.append("BSPline");
+            break;
+        case 3:
+            Interpolator = GaussianInterpolationType::New();
+            OutputFilename.append("Gaussian");
+            break;
+        default:
+            std::cout << "Value not handled - abort";
+            return EXIT_FAILURE;
         }
-        ResampleImageFilter->Modified();
-    }
-    durations.erase(durations.begin(), durations.begin() + 5);
 
-    std::ofstream outstream{ OutputFilename.append(".txt"), std::ios::out | std::ios::trunc };
-
-    if (outstream.is_open())
-    {
-        for each (auto& datapoint in durations)
+        if (dynamic_cast<itk::GaussianInterpolateImageFunction<ImageType, double>*>(Interpolator.GetPointer()) != nullptr)
         {
-            outstream << datapoint << std::endl;
+            auto nInterpolator = dynamic_cast<itk::GaussianInterpolateImageFunction<ImageType, double>*>(Interpolator.GetPointer());
+            nInterpolator->SetSigma(0.8);
+            Interpolator = nInterpolator;
         }
-        outstream.close();
+
+        ResampleImageFilter->SetInterpolator(Interpolator);
+
+        std::vector<long long> durations(105);
+        for (long long& duration : durations)
+        {
+            {
+                auto Timer{ HighPrecisionTimer<TimeUnits::Milliseconds, false>(&duration) };
+                TRY_AND_EXIT_ON_ITK_EXCEPTION(ResampleImageFilter->Update());
+            }
+            ResampleImageFilter->Modified();
+        }
+        durations.erase(durations.begin(), durations.begin() + 5);
+
+        std::ofstream outstream{ std::string{OutputFilename}.append(".txt"), std::ios::out | std::ios::trunc };
+
+        if (outstream.is_open())
+        {
+            for each (auto& datapoint in durations)
+            {
+                outstream << datapoint << std::endl;
+            }
+            outstream.close();
+        }
+
+        WriteImageFile<ImageType>(ResampleImageFilter->GetOutput(), OutputFilename);
+
+        auto GTReader = itk::ImageFileReader<ImageType>::New();
+        GTReader->SetFileName(argv[2]);
+
+        auto SubtractionImageFilter = itk::SubtractImageFilter<ImageType>::New();
+        SubtractionImageFilter->SetInput1(ResampleImageFilter->GetOutput());
+        SubtractionImageFilter->SetInput2(GTReader->GetOutput());
+        auto AbsImageFilter = itk::AbsImageFilter<ImageType, ImageType>::New();
+        AbsImageFilter->SetInput(SubtractionImageFilter->GetOutput());
+        TRY_AND_EXIT_ON_ITK_EXCEPTION(AbsImageFilter->Update());
+
+        WriteImageFile<itk::Image<double, 2>>(Extract2DImage(AbsImageFilter->GetOutput()), OutputFilename, "Sub");
+        WriteImageFile<itk::Image<double, 2>>(Extract2DImage(GTReader->GetOutput()), "GroundTruth", "2D");
     }
-
-    WriteImageFile(ResampleImageFilter->GetOutput(), OutputFilename);
-
-    auto GTReader = itk::ImageFileReader<ImageType>::New();
-    GTReader->SetFileName(argv[2]);
-
-    auto SubtractionImageFilter = itk::SubtractImageFilter<ImageType>::New();
-    SubtractionImageFilter->SetInput1(ResampleImageFilter->GetOutput());
-    SubtractionImageFilter->SetInput2(GTReader->GetOutput());
-
-    TRY_AND_EXIT_ON_ITK_EXCEPTION(SubtractionImageFilter->Update());
-
-    WriteImageFile(SubtractionImageFilter->GetOutput(), OutputFilename, "Sub" );
-   
     return EXIT_SUCCESS;
 }
 
-void WriteImageFile(const ImageType::Pointer& image, std::string outputname, std::string additionalInformation)
+template<class TImage>
+void WriteImageFile(typename TImage::Pointer image, std::string outputname, std::string additionalInformation)
 {
     if (!additionalInformation.empty())
     {
@@ -133,9 +143,24 @@ void WriteImageFile(const ImageType::Pointer& image, std::string outputname, std
     }
 
     outputname.append(".nrrd");
-    using WriterType = itk::ImageFileWriter<ImageType>;
-    WriterType::Pointer writer = WriterType::New();
+    using WriterType = itk::ImageFileWriter<TImage>;
+    typename WriterType::Pointer writer = WriterType::New();
     writer->SetFileName(outputname);
     writer->SetInput(image);
     TRY_AND_EXIT_ON_ITK_EXCEPTION(writer->Update())
+}
+
+auto Extract2DImage(ImageType::Pointer image)-> itk::Image<double,2>::Pointer
+{
+    auto ExtractImageFilter = ExtractImageFilterType::New();
+    auto region{ image->GetLargestPossibleRegion() };
+    auto size = region.GetSize();
+    size[2] = 0;
+    region.SetSize(size);
+    ExtractImageFilter->SetExtractionRegion(region);
+    ExtractImageFilter->SetDirectionCollapseToIdentity();
+    ExtractImageFilter->SetInput(image);
+    TRY_AND_EXIT_ON_ITK_EXCEPTION(ExtractImageFilter->Update());
+
+    return ExtractImageFilter->GetOutput();
 }
